a7b33a92b4563617de20576c94a51a10bb394d72
ODN418 <76906971+ODN418@users.noreply.github.com>
2022-06-07 15:22:29 +0900

    アプリ化の検討開始

---
+++ Tracking/app.py
@@ -0,0 +1,27 @@
+import cv2
+from keras.models import load_model
+import numpy as np
+cap = cv2.VideoCapture(0)
+
+keras_param = "./cnn.h5"
+model = load_model(keras_param)
+
+while True:
+    ret, frame = cap.read()
+
+    prd = model.predict(np.array([frame]))
+    prelabel = np.argmax(prd, axis=1)
+    if prelabel == 0:
+        text = 'p'
+        font = cv2.FONT_HERSHEY_PLAIN
+                
+    # 表示
+    cv2.imshow("Show FLAME Image", frame) 
+    # qを押したら終了。
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+ 
+# 後始末。しなくても終わる。
+cap.release()
+cv2.destroyAllWindows()
